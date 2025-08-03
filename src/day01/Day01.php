00ee74c471227607d1ab9be561359e8fda739c08
carl <carlstromberg3@gmail.com>
2025-08-03 18:33:12 +0300

    day01 part 2 completed with an execution time of 0.091650009155273 seconds

--- src/day01/Day01.php
+++ src/day01/Day01.php
@@ -7,6 +7,8 @@ namespace Advent_Of_Code\day01;
 class Day01
 {
     public function solvePart1(string $input): int {
+        $startTime = microtime(true);
+
         $array = explode(PHP_EOL, $input);
 
         foreach ($array as $value) {
@@ -31,7 +33,52 @@ class Day01
             }
         }
 
-        print_r($distance);
+        print_r($distance . PHP_EOL);
+
+        $endTime = microtime(true);
+
+        $executionTime = $endTime - $startTime;
+
+        print_r($executionTime);
+
+        return 0;
+    }
+
+    public function solvePart2(string $input): int {
+        $startTime = microtime(true);
+
+        $array = explode(PHP_EOL, $input);
+
+        foreach ($array as $value) {
+            $array2[] = explode('  ', $value);
+        }
+
+        foreach ($array2 as $value) {
+            $firstArray[] = $value[0];
+            $secondArray[] = $value[1];
+        }
+
+        $numberFrequency[] = [];
+        foreach ($firstArray as $key => $value) {
+            foreach ($secondArray as $key2 => $value2) {
+                if ((int)$value === (int)$secondArray[$key2]) {
+                    $numberFrequency[$value] += 1; 
+                }
+            }
+        }
+
+        $result = 0;
+        foreach ($numberFrequency as $key => $value) {
+            $result += $key * (int)$value;
+        }
+
+        print_r($result . PHP_EOL);
+
+        $endTime = microtime(true);
+
+        $executionTime = $endTime - $startTime;
+
+        print_r($executionTime);
 
         return 0;
     }
